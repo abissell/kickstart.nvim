30757c0e06af33ac81b2db1cbb8cf3fab57207b0
Andrew Bissell <abissell@drwholdings.com>
2024-03-31 02:08:46 +0000

    add solargraph and sorbet to LSPs

--- init.lua
+++ init.lua
@@ -565,6 +565,22 @@ require('lazy').setup({
             },
           },
         },
+        solargraph = {
+          cmd = { os.getenv 'HOME' .. '/.rbenv/shims/solargraph', 'stdio' },
+          root_dir = require('lspconfig').util.root_pattern('Gemfile', '.git', '.'),
+          settings = {
+            solargraph = {
+              autoformat = true,
+              completion = true,
+              diagnostic = true,
+              folding = true,
+              references = true,
+              rename = true,
+              symbols = true,
+            },
+          },
+        },
+        sorbet = {},
       }
 
       -- Ensure the servers and tools above are installed
